e84a671734c41e4890fa1ed6efb99f3c6598199b
Connor Ward <cward@student.neumont.edu>
2025-08-27 10:56:43 -0600

    This is the updated version with the weather Widget

--- weatherwidget/weather.js
+++ weatherwidget/weather.js
@@ -1,12 +1,43 @@
-const apiKey = "23a12995ae59e4e52a0fbf5079a9e3a5";
+const apiKey = "11cddde5c5e5efdcd45e14b09d41b4c9";
 
-document.getElementById("getWeatherBTn")?.addEventListener("click", () => {
+document.getElementById("getWeatherBtn")?.addEventListener("click", () => {
     const city = document.getElementById("cityInput").value;
     if (city) {
-        localStorage.setitem("lastCity", city);
-        fetchTodayWeather(city);
+        localStorage.setItem("lastCity", city);
+        fetchForecast(city);
     }
 });
 
-function fetchTodayWeather(city) {
-    fetch("https://")
+function fetchForecast(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`)
+        .then (res => res.json())
+        .then (data => {
+            console.log(data);
+
+            if (data.cod !== "200") {
+                alert(`Error: ${data.message}`);
+                return;
+            }
+
+            const forecastContainer = document.getElementById("forecastDisplay");
+            forecastContainer.innerHTML = "";
+
+            for (let i = 0; i < data.list.length; i += 8) {
+                const day = data.list[i];
+                forecastContainer.innerHTML += `
+                  <div class="weather-card">
+                    <h3>${new Date(day.dt_txt).toLocaleDateString()}</h3>
+                    <p>${day.weather[0].description}</p>
+                    <p>Temp: ${day.main.temp} °F</p>
+                    <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png">
+                    </div>
+                `;
+            }
+        })
+        .catch(() => alert("City Not Found"));
+    }
+
+    window.addEventListener("load", () => {
+        const lastCity = localStorage.getItem("lastCity");
+        if (lastCity) fetchForecast(lastCity);
+    });
